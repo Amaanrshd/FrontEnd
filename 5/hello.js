a018f97be87d2ebd3b03aabf34770bed9500f47e
Amaanrshd <amaanrasheed14@gmail.com>
2025-08-13 18:30:23 +0400

    Add files via upload

---
+++ 5/hello.js
@@ -0,0 +1,61 @@
+// for (let i=1; i <=10; i++) {                         1 - 10
+//     console.log(i)}
+//for (let i=10; i >= 1;i--){                           10 - 1
+//}
+
+// let i = 1;
+// while (i<=10) {
+// ....;
+// i++1}
+
+
+//q1
+
+// let balance = 5000;
+// let withdraw = 500;
+// while (balance>=500 ){
+//     balance -= withdraw;
+//     if (balance <500){
+//         console.log('Insufficient balance');
+//         break;}
+//     else {
+//         console.log('Current balance: ' + balance);
+
+//     }
+// }
+
+
+//q4
+// let light= 'red';
+// if (light === 'red') {
+//     console.log('Stop');
+// }
+// else if (light === 'yellow') {
+//     console.log('Slow down');
+// }
+// else if (light === 'green') {
+//     console.log('Go');
+// }
+
+//q5
+
+// let bill = 100;
+// let service = ('excellent')
+// if (service === 'excellent') {
+//     console.log('20% tip added! Your total is: ' + (bill + (bill * 0.2)));
+// }
+// else if (service === 'good') {
+//     console.log('10% tip added! Your total is: ' + (bill + (bill *0.10)));
+// }
+// else {
+//     console.log('0% tip added! Your total is: ' + bill);
+// }
+
+//Loop q1
+
+// console .log (" Rocket Launch Countdown :") ;
+// for (let i = 10; i >= 1; i - -) {
+//     console .log (i + "... ") ;
+// }
+// console .log (" BLAST OFF !") ;
+
